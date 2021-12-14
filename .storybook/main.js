e860aa5d13ca86a59487c1d15a6e94593bdc791a
aelbore <aelbore.sg@gmail.com>
2021-12-14 17:03:49 +0800

    update vite-lit plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,28 +2,33 @@ const litcss = require('rollup-plugin-postcss-lit')
 const inlineLit = require('rollup-plugin-lit-transformer')
 const path = require('path')
 
+const stories = (root) => 
+  path.join(path.relative(
+    root,
+    path.resolve(__dirname, '../stories')
+  ), '**/*.stories.ts')
+
 const config = {
   framework: "@storybook/web-components",
   stories: [ '../stories/**/*.stories.@(js|jsx|ts|tsx)' ],
   core: {
     "builder": 'storybook-builder-vite'
   },
-  async viteFinal(config) {
+  viteFinal(config, { configType }) {
+    const env = configType.toLowerCase()
     config.esbuild = false
-    config.plugins.push(inlineLit.viteLit())
+    config.plugins.push(inlineLit.viteLit({ 
+      env,
+      minifyHTMLLiterals: env.includes('production')
+    }))
     config.plugins.push(litcss())
     config.optimizeDeps = {
       ...config.optimizeDeps,
       include: [
-        ...config.optimizeDeps.include,
+        ...config?.optimizeDeps?.include || [],
         '@storybook/web-components'
       ],
-      entries: [
-        `${path.relative(
-            config.root, 
-            path.resolve(__dirname, '../stories')
-          )}/**/*.stories.ts`
-      ]
+      entries: [ stories(config.root) ]
     }
     return config
   }
